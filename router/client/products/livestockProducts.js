23d633053a9c3bd277121be6c682b5102fb1b357
OmoniyiIfeAbraham <86631380+OmoniyiIfeAbraham@users.noreply.github.com>
2022-12-25 16:58:36 +0100

    Update

--- router/client/products/livestockProducts.js
+++ router/client/products/livestockProducts.js
@@ -5,7 +5,7 @@ const productsMod = require('./../../../models/products/products')
 
 router.get('/', async(req, res) => {
     const products = await productsMod.find({ category: 'livestock product'})
-    res.render('client/products/livestockproducts', { products, msg: '', resp: '' })
+    res.render('client/products/livestockProducts', { products, msg: '', resp: '' })
 })
 
 module.exports = router
